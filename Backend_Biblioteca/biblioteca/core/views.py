cffb3bed93749eb6423bacf3eed67ac30b4d5960
igorpereirag <igorpereira1501@gmail.com>
2024-08-20 16:01:25 -0300

    chore: adicionando mensagens de erro personalizadas

--- Backend_Biblioteca/biblioteca/core/views.py
+++ Backend_Biblioteca/biblioteca/core/views.py
@@ -1,10 +1,9 @@
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.response import Response
+from django.http import JsonResponse 
+from rest_framework.parsers import JSONParser
 from rest_framework import status
 from .models import Livro
 from .serializers import LivroSerializer
-import json
-from django.http import JsonResponse, HttpResponse
 
 @csrf_exempt
 def livro_list_create(request):
@@ -13,47 +12,33 @@ def livro_list_create(request):
         serializer = LivroSerializer(livros, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-        except json.JSONDecodeError:
-            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
-        
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
         serializer = LivroSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-   
-    return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @csrf_exempt
 def livro_detail(request, pk):
     try:
         livro = Livro.objects.get(pk=pk)
     except Livro.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-    
+        return JsonResponse({'error': 'Livro não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
     if request.method == 'GET':
         serializer = LivroSerializer(livro)
         return JsonResponse(serializer.data)
-    
-    if request.method == 'PUT':
-        try:
-            data = json.loads(request.body)
-        except json.JSONDecodeError:
-            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
-        
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
         serializer = LivroSerializer(livro, data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    if request.method == 'DELETE':
+
+    elif request.method == 'DELETE':
         livro.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
-    
-    
-    return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return JsonResponse({'message': 'Livro deletado com sucesso.'}, status=status.HTTP_204_NO_CONTENT)
