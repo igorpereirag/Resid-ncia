d4c2cf800b7d2e4aaefeb9a5c7dae680b29842cd
igorpereirag <igorpereira1501@gmail.com>
2024-10-08 15:24:40 -0300

    chore: change serializers

--- Backend_Biblioteca/biblioteca/core/serializers.py
+++ Backend_Biblioteca/biblioteca/core/serializers.py
@@ -1,13 +1,23 @@
 from rest_framework import serializers
-from .models import Categoria, Autor, Livro
+from .models import Livro, Autor, Categoria
+
+class AutorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = ['id', 'nome']
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = ['id', 'nome']
 
 class LivroSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    titulo = serializers.CharField(max_length=200) 
-    autor = serializers.CharField(source='autor.nome', read_only=True) 
-    categoria = serializers.CharField(source='categoria.nome', read_only=True) 
+    titulo = serializers.CharField(max_length=200)
+    autor = serializers.CharField(source='autor.nome', read_only=True)
+    categoria = serializers.CharField(source='categoria.nome', read_only=True)
     autor_id = serializers.PrimaryKeyRelatedField(queryset=Autor.objects.all(), write_only=True)
-    categoria_id = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all(), write_only=True)  
+    categoria_id = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all(), write_only=True)
     publicado_em = serializers.DateField()
 
     def create(self, validated_data):
