ba9e3feceb028d21d9dd45c5b64eda5a7d95c367
igorpereirag <igorpereira1501@gmail.com>
2024-10-08 19:19:02 -0300

    chore: fix urlpatterns

--- Backend_Biblioteca/biblioteca/core/urls.py
+++ Backend_Biblioteca/biblioteca/core/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    path('livros/', views.LivroList.as_view(), name='livros-list'),
-    path('livros/<int:pk>/', views.LivroDetail.as_view(), name='livro-detail'),
-    path('autores/', views.AutorList.as_view(), name='autores-list'),
-    path('autores/<int:pk>/', views.AutorDetail.as_view(), name='autor-detail'),
-    path('categorias/', views.CategoriaList.as_view(), name='categorias-list'),
-    path('categorias/<int:pk>/', views.CategoriaDetail.as_view(), name='categoria-detail'),
+ path('livros/', views.LivroList.as_view(), name='livros-list'),
+ path('livros/<int:pk>/', views.LivroDetail.as_view(), name='livrodetail'),
 ]
